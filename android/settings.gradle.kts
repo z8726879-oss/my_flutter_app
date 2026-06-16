3a37385cb52972fa90181c791beedc5bce31102f
zoro <Z8726879-OSS.com>
2026-06-16 12:56:47 +0300

    fix settings.gradle.kts syntax

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,12 +16,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 plugins {
-    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.4.0" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.10" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+
 
 
 include(":app")
