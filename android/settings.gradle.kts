9143559a0930755df10c07e30bd1c79ee1f0f722
zoro <Z8726879-OSS.com>
2026-06-16 12:39:14 +0300

    Final unified build workflow

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,9 +18,10 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.4.0" apply false
+    id "org.jetbrains.kotlin.android" version "1.9.10" apply false
 }
+
 
 include(":app")
